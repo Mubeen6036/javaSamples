08795326e446714754940d025611d47c45c4ddd6
Mubeen6036 <s.mubeenmusthafa@gmail.com>
2021-05-13 00:10:34 +0530

    Second commit

--- Samples/src/ThreadExample.java
+++ Samples/src/ThreadExample.java
@@ -3,7 +3,11 @@ public class ThreadExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Runnable r = ()->{
+			System.out.println("Hello Bitch");
+		};
+		Thread t = new Thread(r);
+		t.start();
 	}
 
 }
