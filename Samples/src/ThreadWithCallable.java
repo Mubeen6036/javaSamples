1665ea8ef8a6ee003b08e91a587373551072d31f
Mubeen6036 <s.mubeenmusthafa@gmail.com>
2021-05-13 19:56:12 +0530

    2nd commit

---
+++ Samples/src/ThreadWithCallable.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+public class ThreadWithCallable {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Callable<Integer> g = ()->{
+			System.out.println("afasdfasdf");
+			return 1;
+		};
+		
+		Integer f = g.call();
+		System.out.println(f);
+	}
+
+}
